2e96962fbfbaaa62acaf3606448601384d1d18a8
SRRahmaaaan <sydurrahman2699@gmai.com>
2021-03-31 22:58:27 +0600

    finsish home responsiveness

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -2,10 +2,12 @@ import React, { useState } from 'react'
 import {Link} from "react-router-dom"
 import logo from "./navbarLogo.png"
 import "./Navbar.css"
-import {Button, Alert} from "react-bootstrap"
+import { Button, Alert } from "react-bootstrap"
+import {FaBars, FaTimes} from "react-icons/fa"
 import { useAuth } from '../Authentication/AuthContext'
 
 const Navbar = () => {
+    const [drawer, setDrawer] = useState(false)
     const { currentUser, logOut } = useAuth()
     const [error, setError] = useState("")
     async function handleLogout () {
@@ -21,14 +23,20 @@ const Navbar = () => {
             <div className="logo">
                 <img src={logo} alt="siteLogo"/>
             </div>
-            <div className="navLinks">
-                <Link to="/">Home</Link>
-                <Link to="/orders">Orders</Link>
-                <Link to="/admin">Admin</Link>
-                {
-                    currentUser ? <Button className="logout-button" variant="dark" onClick={handleLogout}>Log Out</Button> :
-                    <Link to="/login">Login</Link>
-                }
+            {
+                drawer ? <FaTimes className="cross" onClick={() => setDrawer(!drawer)} /> :
+                <FaBars className="bars" onClick={() => setDrawer(!drawer)} />
+            }
+            <div className="navLinks" style={{transform: drawer? "translateX(0%)" : "translateX(100%)"}}>
+                <div className="navLinks2">
+                    <Link to="/">Home</Link>
+                    <Link to="/orders">Orders</Link>
+                    <Link to="/admin">Admin</Link>
+                    {
+                        currentUser ? <Button className="logout-button" variant="dark" onClick={handleLogout}>Log Out</Button> :
+                        <Link to="/login">Login</Link>
+                    }
+                </div>
             </div>
 
         </nav>
